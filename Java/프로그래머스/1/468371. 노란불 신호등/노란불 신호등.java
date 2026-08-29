c44b40b5344dfdc6b4fa0ed6abd0e566a39270b4
Hyun-suck Lee <144207343+leehyunsuck@users.noreply.github.com>
2026-08-29 14:11:57 +0900

    add comment

--- Java/프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
+++ Java/프로그래머스/1/468371. 노란불 신호등/노란불 신호등.java
@@ -9,17 +9,22 @@ class Solution {
     
     public int solution(int[][] signals) {
         // LCM 계산 수식 모름 ㅇㅅㅇ...
-        int max = 20 * 20 * 20 * 20 * 20;
-        
+        // 이거는 수학을 몰라서 아몰랑 해버렸는데 통과 되더라고 (1레벨 문제라 최적화까지 요구 안했나봄)
+        int max = 20 * 20 * 20 * 20 * 20;   
+
+        // max + 1 한 이유는 이중for문 안에 보면 timeSlice를 계산 편하게 1 부터 시작하게 했기 때문
         int[] yellowTimes = new int[max + 1];
         
         for (int idx = 0; idx < signals.length; idx++) {
             int green  = signals[idx][0],
                 yellow = signals[idx][1],
                 red    = signals[idx][2];
-            
+
+            // yellow 구간만 계산했음
             for (int timeSlice = 1; timeSlice <= max;) {
-                if (timeSlice + green + yellow > max) break;
+                // 이거 addCounting에 최대값 넘는경우에 대한 차단을 안해서 여기서 차단했는데
+                // 생각해보니 여기 말고 addCounting에서 하는게 맞을듯?
+                if (timeSlice + green + yellow > max) break; 
                 timeSlice += green;
                 addCounting(yellowTimes, timeSlice, yellow);
                 timeSlice += yellow + red;
