094767582da81d3edfafe6541be847a848c63fd4
Hyun-suck Lee <144207343+leehyunsuck@users.noreply.github.com>
2026-09-06 17:42:21 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 16.16 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/Hyun-suck Lee/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/Hyun-suck Lee/가장 많이 받은 선물.java
@@ -1,3 +1,4 @@
+// 태성씨 코드 보고 초기화 로직 및 giftSocre 2차 로직 수정 
 import java.util.*;
 
 class Solution {
@@ -7,35 +8,18 @@ class Solution {
         // 사람 : 선물 지수
         Map<String, Integer> giftScore = new HashMap<>();
         
-        for (String userA : friends) {
-            Map<String, Integer> giftCount = new HashMap<>();
-            
-            for (String userB : friends) {
-                if (userA.equals(userB)) continue;
-                giftCount.put(userB, 0);
-            }
-            
-            giftForMe.put(userA, giftCount);
-            giftScore.put(userA, 0);
-        }
-        
         for (String gift : gifts) {
             String[] whoToWhom = gift.split(" ");
             String who  = whoToWhom[0],
                    whom = whoToWhom[1];
             
             giftScore.merge(who, 1, Integer::sum);
-            giftForMe.get(whom).merge(who, 1, Integer::sum);    // 준 개수 먼저 종합
-        }
-        
-        // giftScore 최종 계산 (받은 개수 빼야 함)
-        for (String user : friends) {
-            int getGiftCount = 0;
-            for (int count : giftForMe.get(user).values()) {
-                getGiftCount -= count;
-            }
+            giftScore.merge(whom, -1, Integer::sum);    
             
-            giftScore.merge(user, getGiftCount, Integer::sum);
+            if (!giftForMe.containsKey(whom)) {
+                giftForMe.put(whom, new HashMap<>());
+            }
+            giftForMe.get(whom).merge(who, 1, Integer::sum);
         }
         
         // 결과 종합
@@ -43,17 +27,21 @@ class Solution {
         for (String userA : friends) {
             int aWillGetGift = 0;
             
-            int aScore = giftScore.get(userA);
+            int aScore = giftScore.getOrDefault(userA, 0);
             
             for (String userB : friends) {
                 if (userA.equals(userB)) continue;
                 
-                int aForB = giftForMe.get(userB).get(userA),
-                    bForA = giftForMe.get(userA).get(userB);
+                int aForB = giftForMe
+                        .getOrDefault(userB, Collections.emptyMap())
+                        .getOrDefault(userA, 0),
+                    bForA = giftForMe
+                        .getOrDefault(userA, Collections.emptyMap())
+                        .getOrDefault(userB, 0);
                 
                 if (aForB < bForA) continue;            // A가 준 개수가 더 적음
                 if (aForB == bForA) {
-                    int bScore = giftScore.get(userB);  
+                    int bScore = giftScore.getOrDefault(userB, 0);  
                     if (aScore <= bScore) continue;     // A 선물 지수가 더 작음
                 }
 
