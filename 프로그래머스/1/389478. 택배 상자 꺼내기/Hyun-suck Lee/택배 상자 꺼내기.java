c9ee8a7c5bfcc6b269e1ba4a2f18dece08a44cc5
Hyun-suck Lee <144207343+leehyunsuck@users.noreply.github.com>
2026-09-07 21:49:48 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/Hyun-suck Lee/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/Hyun-suck Lee/택배 상자 꺼내기.java
@@ -1,21 +1,17 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int result = 1,
-            numCol = -1; // num에 해당하는 column 인덱스 쓸거임
+        int result = 1;
         
-        int nowNum = 1;
-        while (nowNum <= n) {
-            // 우측으로
-            for (int col = 0; col < w; col++) {
-                if (nowNum > n) return result;
-                if (numCol == col) result++;
-                if (nowNum++ == num) numCol = col;
-            }
-            // 좌측으로
-            for (int col = w - 1; col >= 0; col--) {
-                if (nowNum > n) return result;
-                if (numCol == col) result++;
-                if (nowNum++ == num) numCol = col;
+        boolean isRight = true;
+        // col 은 nowNum 인덱스고 numCol은 num에 해당하는 인덱스임
+        for (int nowNum = 1, col = 0, numCol = -1; nowNum <= n; nowNum++) {
+            if (numCol == col) result++;
+            if (nowNum == num) numCol = col;
+            
+            col += isRight ? 1 : -1;
+            if (col == -1 || col == w) {
+                isRight = !isRight;
+                col += isRight ? 1 : -1;    // 끝쪽은 같은 인덱스 한번 더 써야됨
             }
         }
         
